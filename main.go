26c63b0619fb24260e1e98f07253888f42d2d31d
onionoino <bowenyang16@foxmail.com>
2023-02-05 22:35:07 +1100

    go mod tidy

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"category/handler"
-	pb "category/proto"
-
-	"github.com/micro/micro/v3/service"
-	"github.com/micro/micro/v3/service/logger"
-)
-
 func main() {
-	// Create service
-	srv := service.New(
-		service.Name("category"),
-	)
 
-	// Register handler
-	pb.RegisterCategoryHandler(srv.Server(), handler.New())
-
-	// Run service
-	if err := srv.Run(); err != nil {
-		logger.Fatal(err)
-	}
 }
